ef488e06b732863cb4da55243d51c79ba601c23f
Gabriel Spindola <gspindola@magnasistemas.com.br>
2022-04-16 23:28:21 -0300

    feat: Adicionando Tabela do Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-import * as C from './App.styles';
+import { useState, useEffect } from "react";
+import * as C from "./App.styles";
+import { Item } from "./types/Item";
+import { Category } from "./types/Category";
+import { categories } from "./data/categorioes";
+import { items } from "./data/items";
+import { getCurrentMonth, filterListByMonth } from "./helpers/dateFilter";
+import { TableArea } from './components/TableArea';
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList( filterListByMonth(list, currentMonth) );
+  }, [list, currentMonth])
+
   return (
     <C.Container>
       <C.Header>
-        <C.HeaderText>
-          Sistema de Controle Financeiro
-        </C.HeaderText>
+        <C.HeaderText>Sistema de Controle Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        
+
+
+
+
+        <TableArea />
+
       </C.Body>
     </C.Container>
   );
-}
+};
 
 export default App;
---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  {
+    date: new Date(2022, 4, 16),
+    category: "food",
+    title: "McDonalds",
+    value: 32.12,
+  },
+  {
+    date: new Date(2022, 4, 16),
+    category: "food",
+    title: "Pizza Hut",
+    value: 40,
+  },
+  {
+    date: new Date(2022, 4, 20),
+    category: "rent",
+    title: "Aluguel do Apartamento",
+    value: 3200,
+  },
+  {
+    date: new Date(2022, 10, 12),
+    category: "salary",
+    title: "Salário",
+    value: 4300,
+  },
+];
